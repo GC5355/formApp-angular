542ac62c65166727759d4d4b7a9df6e1b8f0ce6e
German <gercger5@gmail.com>
2024-08-13 17:39:34 -0300

    second commit

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './pages/register/register.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children:[
+      {path: 'sing-up', component: RegisterComponent},
+      {path: '**', redirectTo: 'sign-up'}
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
