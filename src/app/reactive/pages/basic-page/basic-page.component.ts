b52047c99fca52762cac80c100015ae486e15472
German <gercger5@gmail.com>
2024-08-14 14:27:02 -0300

    Fin seccion 17

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+// Me manda de la BD
+const rtx5090 = {
+  name: 'RTX 5090',
+  price: 1500,
+  inStorage: 50
+}
 
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html'
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
+
 
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl('', [], []),
@@ -14,20 +22,61 @@ export class BasicPageComponent {
   // });
 
   public myForm: FormGroup = this.fb.group({
-    name: ['', [ Validators.required, Validators.minLength(3) ]],
-    price: [0, [Validators.required, Validators.min(0) ]],
-    inStorage: [0,  [Validators.required, Validators.min(0) ]],
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    inStorage: [0, [Validators.required, Validators.min(0)]],
   })
 
 
-  constructor( private fb: FormBuilder ){}
+  constructor(private fb: FormBuilder) { }
+
 
   onSave(): void {
-
-    if( this.myForm.invalid) return;
-
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
     console.log(this.myForm.value)
 
+    this.myForm.reset({ price: 10, inStorage: 0 });
   }
 
+  ngOnInit(): void {
+    // Establezco el valor inicial del form
+    // this.myForm.reset( rtx5090);
+  }
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched
+  }
+
+
+  getFieldError( field: string ): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo ${ errors['minlength'].requiredLength } caracters.`;
+      }
+    }
+
+    return null;
+  }
+
+
+
+
+
+
+
+
 }
+
